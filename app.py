932af1bea9b13df911c3918d55c8aaaa94549988
issac1806-lgbt <issac1806@gmail.com>
2025-11-06 16:54:47 +0800

    Update app.py

--- app.py
+++ app.py
@@ -2778,4 +2778,7 @@ def get_initials(name):
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
-    socketio.run(app, host='0.0.0.0', port=port, debug=False)
+    if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    # Use this for production
+    socketio.run(app, host='0.0.0.0', port=port, debug=False, allow_unsafe_werkzeug=True)
